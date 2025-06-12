f62ea0b6caad57f95cb8611b2664df5b49c5cdad
Daniel-Ferreira-2004 <danikferreira69@gmail.com>
2025-06-12 16:52:56 -0300

    Controller Criado

---
+++ MinhaAPI/Controllers/ProdutosController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MinhaAPI.Context;
+using MinhaAPI.Models;
+
+namespace MinhaAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+            if(produtos is null)
+            {
+                return NotFound("Produtos não encontrados");
+            }
+            return produtos;
+        }
+    }
+}
